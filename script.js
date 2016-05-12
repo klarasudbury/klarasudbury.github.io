1bb10d85587c3afe27213170fb542873b39b5339
Vaeril <marleysudbury@gmail.com>
2016-05-12 22:55:29 +0100

    Revert "Facebook, CSS"
    This reverts commit b5b70e583b6231b04d22dd4992e4a458830a032f.

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("nav01").innerHTML =
-"<nav>"+
-  "<li><a href='http://klarasudbury.co.uk/'>Home</a></li> "+
-  "<li><a href='http://klarasudbury.co.uk/images'>Images</a></li>";
